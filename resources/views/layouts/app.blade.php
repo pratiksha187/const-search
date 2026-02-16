031474f1eab008bcab06110549402c806a5075c5
pratiksha187 <pratikshanmisal@gmail.com>
2026-02-16 11:16:16 +0530

    chnages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'My Website')</title>
+<link rel="manifest" href="/manifest.json">
+<meta name="theme-color" content="#f25c05">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -38,5 +40,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     @stack('scripts')
+    <script>
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(function() {
+        console.log('Service Worker Registered');
+    });
+}
+</script>
+
 </body>
 </html>
