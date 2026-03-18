9e15f40451cf9ff50d0b7b427512032cb74f95f9
pratiksha187 <pratikshanmisal@gmail.com>
2026-03-18 12:37:06 +0530

    chnages

--- app/Http/Controllers/RazorpayController.php
+++ app/Http/Controllers/RazorpayController.php
@@ -14,6 +14,7 @@ class RazorpayController extends Controller
 {
     public function createOrder(Request $request)
     {
+        dd($request);
         $request->validate([
             'cust_id' => 'required',
             'plan'    => 'required',
